c2f284bb90cd1c457b1adc26eb6efd16d147063d
krishnachandran-u <kriss52450@gmail.com>
2023-12-14 19:01:30 +0530

    new project #1

--- src/preloader/preloader.js
+++
@@ -1,8 +0,0 @@
-function updateScreenWidth() {
-    var screenwidth = window.innerWidth; 
-    console.log('Screen width: ' + screenwidth + 'pixels');
-}
-
-window.addEventListener('resize', updateScreenWidth);
-
-updateScreenWidth();
