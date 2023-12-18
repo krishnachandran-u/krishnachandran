36af1c28898b19c651a9af2e549555215949b233
krishnachandran-u <kriss52450@gmail.com>
2023-12-18 18:11:01 +0530

    create NEXT.JS project

--- docs/scripts/misc.js
+++
@@ -1,8 +0,0 @@
-function printViewportSize() {
-    console.log("Viewport Width: " + window.innerWidth);
-    console.log("Viewport Height: " + window.innerHeight);
-}
-
-window.addEventListener("resize", printViewportSize);
-
-printViewportSize();
