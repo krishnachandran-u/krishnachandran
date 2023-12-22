5ca171bf4d8482b5592051c3c03b4cdc88cd2b0d
unknown <kriss52450@gmail.com>
2023-12-22 13:17:37 +0530

    add professionLine

--- src/components/One.tsx
+++ src/components/One.tsx
@@ -4,6 +4,14 @@ const One = () => {
     return (
         <>
             <div className = {styles.root}>
+                <div className = {styles.nameLine}>
+                    <h2 className = {styles.hi}>HI, I'M&nbsp;</h2>
+                    <h2>KRISHNACHANDRAN&nbsp;</h2>
+                    <h2 className = {styles.hi}>AND I AM A</h2>
+                </div>
+                <div className={styles.professionLine}>
+                    <h1>FULL STACK DEVELOPER <br /> & DATA ENGINEEER</h1>
+                </div>
             </div>
         </>
     );
