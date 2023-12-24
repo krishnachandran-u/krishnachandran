8ae2fae0c27ac9b8e44eeed777398fa3e534b2dd
unknown <kriss52450@gmail.com>
2023-12-24 21:20:47 +0530

    page2

--- src/components/Two.tsx
+++ src/components/Two.tsx
@@ -1,16 +1,57 @@
 import styles from './../styles/Two.module.scss';
 import StackIcon from './../components/StackIcon';
+
 import { FaReact } from "react-icons/fa";
 import { SiCplusplus } from "react-icons/si";
+import { SiC } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { BiLogoTypescript } from "react-icons/bi";
+import { FaSass } from "react-icons/fa";
+import { SiNextdotjs } from "react-icons/si";
+import { DiJqueryLogo } from "react-icons/di";
+import { SiTailwindcss } from "react-icons/si";
+import { SiPrisma } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiMongodb } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { SiPhp } from "react-icons/si";
 
 const Two = () => {
     return (
         <>
             <div className={styles.root}>
                 <h1>WHAT DO I KNOW?</h1>
-                <div className = {styles.stacks}>
-                    <StackIcon icon = {FaReact} IconName = "REACT"/>
-                    <StackIcon icon = {SiCplusplus} IconName = "C++"/>
+                <div className = {styles.webdev}>
+                    <h2>WEB DEVELOPMENT</h2>
+                    <div className = {styles.stacks}>
+                        <StackIcon icon={FaHtml5} IconName="HTML" />
+                        <StackIcon icon={IoLogoJavascript} IconName="JAVASCRIPT" />
+                        <StackIcon icon={DiJqueryLogo} IconName="JQUERY" />
+                        <StackIcon icon={FaNodeJs} IconName="NODE.JS" />
+                        <StackIcon icon={BiLogoPostgresql} IconName="POSTGRESQL" />
+                        <StackIcon icon={SiPrisma} IconName="PRISMA" />
+                        <StackIcon icon={FaReact} IconName="REACT" />
+                        <StackIcon icon={SiMongodb} IconName="MONGODB" />
+                        <StackIcon icon={SiNextdotjs} IconName="NEXT.JS" />
+                        <StackIcon icon={SiTailwindcss} IconName="TAILWIND CSS" />
+                        <StackIcon icon={FaCss3Alt} IconName="CSS" />
+                        <StackIcon icon={FaSass} IconName="SASS" />
+                        <StackIcon icon={BiLogoTypescript} IconName="TYPESCRIPT" />
+                    </div>
+                </div>
+                <div className = {styles.webdev}>
+                    <h2>SOFTWARE DEVELOPMENT</h2>
+                    <div className = {styles.stacks}>
+                        <StackIcon icon={SiCplusplus} IconName="C++" />
+                        <StackIcon icon={SiC} IconName="C" />
+                        <StackIcon icon={FaPython} IconName="PYTHON" />
+                        <StackIcon icon={FaJava} IconName="JAVA" />
+                        <StackIcon icon={SiPhp} IconName="PHP" />
+                    </div>
                 </div>
             </div>
         </>
