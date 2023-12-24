bbac9bf1ee378cc7c75fc95ba0a9816d61e1db15
unknown <kriss52450@gmail.com>
2023-12-24 11:36:31 +0530

    install jquery and owl.carousel

---
+++ src/components/StacksOwlCarousel.tsx
@@ -0,0 +1,25 @@
+import 'owlcarousel/owl.carousel.min.css';
+import 'owlcarousel/owl.theme.default.min.css';
+
+const StacksOwlCarousel = () => {
+    return (
+        <div>
+            <div className="owl-carousel owl-theme">
+                <div className="item"><h4>1</h4></div>
+                <div className="item"><h4>2</h4></div>
+                <div className="item"><h4>3</h4></div>
+                <div className="item"><h4>4</h4></div>
+                <div className="item"><h4>5</h4></div>
+                <div className="item"><h4>6</h4></div>
+                <div className="item"><h4>7</h4></div>
+                <div className="item"><h4>8</h4></div>
+                <div className="item"><h4>9</h4></div>
+                <div className="item"><h4>10</h4></div>
+                <div className="item"><h4>11</h4></div>
+                <div className="item"><h4>12</h4></div>
+            </div>
+        </div>
+    );
+}
+
+export default StacksOwlCarousel;
--- src/components/Two.tsx
+++ src/components/Two.tsx
@@ -1,10 +1,12 @@
 import styles from './../styles/Two.module.scss';
+import StacksOwlCarousel from './StacksOwlCarousel';
 
 const Two = () => {
     return (
         <>
             <div className = {styles.root}>
-                hi iam kc
+                <div className = {styles.carouselDiv}>
+                </div>
             </div>
         </>
     );
