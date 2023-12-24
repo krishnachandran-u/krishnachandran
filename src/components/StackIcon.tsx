afd33b722f07ec45f3100ff953118eba17ea5d4a
unknown <kriss52450@gmail.com>
2023-12-24 17:23:11 +0530

    set page two skeleton

--- src/components/StackIcon.tsx
+++ src/components/StackIcon.tsx
@@ -1,1 +1,18 @@
-import styles from './../styles/Stackicon.module.scss';
+import styles from './../styles/StackIcon.module.scss';
+import React, { ReactElement, FC } from 'react';
+
+interface StackIconProps {
+    icon: React.ComponentType<{size?: string, color?: string}>
+    IconName: string;
+}
+
+const StackIcon: FC<StackIconProps> = ({icon: IconComponent, IconName}) => {
+    return (
+        <div className = {styles.root}>
+            <IconComponent size = "10vmin" color = "#ffffff"/>
+            <p className = {styles.name}>{IconName}</p>
+        </div>
+    );
+}
+
+export default StackIcon;
--- src/components/Two.tsx
+++ src/components/Two.tsx
@@ -1,9 +1,17 @@
 import styles from './../styles/Two.module.scss';
+import StackIcon from './../components/StackIcon';
+import { FaReact } from "react-icons/fa";
+import { SiCplusplus } from "react-icons/si";
 
 const Two = () => {
     return (
         <>
             <div className={styles.root}>
+                <h1>WHAT DO I KNOW?</h1>
+                <div className = {styles.stacks}>
+                    <StackIcon icon = {FaReact} IconName = "REACT"/>
+                    <StackIcon icon = {SiCplusplus} IconName = "C++"/>
+                </div>
             </div>
         </>
     );
