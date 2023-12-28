f1edd4fbcc2515deb8674017643415f0957e05c6
unknown <kriss52450@gmail.com>
2023-12-28 11:35:51 +0530

    make the first page only fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,20 @@ import styles from './../styles/Main.module.scss';
 
 import One from '../components/One';
 import Two from '../components/Two';
+import Three from '../components/Three';
 
 const Page = () => {
   return (
     <>
       <main id = "mainContainer">
-        <section>
+        <section id = "hero">
           <One />
         </section>
-        <section>
+        <section id = "section">
           <Two />
+        </section>
+        <section>
+          <Three />
         </section>
       </main>
     </>
---
+++ src/components/Three.tsx
@@ -0,0 +1,11 @@
+import styles from './../styles/Three.module.scss';
+
+export default function Three() {
+    return (
+        <>
+            <main className = {styles.root}>
+
+            </main>
+        </>
+    )
+}
