81e74bd4a88b02c15bf053325ddcd651a0db3f35
unknown <kriss52450@gmail.com>
2023-12-29 10:04:22 +0530

    configure nextjs but with tailwind

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-const path = require('path');
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
