e429f1457ec9008841e1fb89041b118c0880c00c
krishnachandran-u <kriss52450@gmail.com>
2024-01-04 21:46:56 +0530

    minor changes + skills page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import ScreenSizeLogger from '@/components/ScreenSizeLogger';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Projects from '@/components/Projects';
+import Skills from '@/components/Skills';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Hero/>
         <About />
         <Projects />
+        <Skills />
     </>
   )
 }
---
+++ src/components/Skills.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Skills.module.scss'
+
+export default function Skills() {
+    return (
+        <div className = {styles.root}>
+            <div className = {styles.main}>
+                <div className = {styles.title}>
+                    MY SKILLS
+                </div> 
+                <div className = {styles.cards}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
