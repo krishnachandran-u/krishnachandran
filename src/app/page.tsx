c1765e8301a3b89a7c3f6896e05bc1a17628d7f3
krishnachandran-u <kriss52450@gmail.com>
2024-02-09 08:32:01 +0530

    UNDER CONSTRUCTION

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,20 @@ import Skills from '@/components/Skills';
 import Connect from '@/components/Connect';
 
 import ParticlesBackground from '@/components/ParticlesBackground';
+import Maintenance from '@/components/Maintenance';
 
 export default function Home() {
   return (
     <>
+        <Maintenance />
+        {/*
         <ParticlesBackground />
         <Hero/>
         <About />
         <Projects />
         <Skills />
         <Connect />
+        */}
     </>
   )
 }
