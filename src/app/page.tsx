9d7853fe7bdc77f406ea6cdf079c88741af29088
Krishnachandran U <kriss52450@gmail.com>
2024-05-24 14:31:35 +0530

    initialise NEXT (old one is boring)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,7 @@
-import './globals.css'; 
-
-import ScreenSizeLogger from '@/components/ScreenSizeLogger';
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import Projects from '@/components/Projects';
-import Skills from '@/components/Skills';
-import Connect from '@/components/Connect';
-
-import ParticlesBackground from '@/components/ParticlesBackground';
-import Maintenance from '@/components/Maintenance';
-
-export default function Home() {
+const Home = () => {
   return (
-    <>
-        <ParticlesBackground />
-        {/*<Maintenance />*/}
-        <Hero/>
-        <About />
-        <Projects />
-        <Skills />
-        <Connect />
-    </>
+    <div>Hi</div>
   )
 }
 
-
+export default Home;
