b3855d5cbd8922f6c62a62e8ef4fe98c4a51f3cf
Krishnachandran U <kriss52450@gmail.com>
2024-05-26 13:33:45 +0530

    minor changes in style of fab

--- src/components/atoms/fab.tsx
+++ src/components/atoms/fab.tsx
@@ -6,11 +6,11 @@ const Fab = () => {
     return (
         <div className = {`p-[16px] ${font.className}`}>
             <div className = "rounded-[16px] border-[1px] shadow-2xl bg-white">
-                <div className = "w-full flex flex-row justify-between xl:items-center md:px-[32px] px-[16px] py-[16px] md:text-[24px] sm:text-[18px] font-semibold">
+                <div className = "w-full flex flex-row justify-between xl:items-center md:px-[32px] px-[16px] py-[16px] md:text-[24px] sm:text-[18px]">
                     <div>
                         Got an idea brewing in your mind? 
                     </div>
-                    <button className = "bg-[#5551ff] text-white py-[10px] px-[20px] rounded-[12px] font-extrabold">
+                    <button className = "bg-[#202124] md:border-[4px] border-[2px] border-[#202124] hover:text-[#202124] hover:bg-white transition-all duration-300 hover:-translate-y-1 text-white py-[10px] px-[20px] rounded-[12px] font-extrabold whitespace-nowrap">
                         Get in touch!
                     </button>
                 </div>
