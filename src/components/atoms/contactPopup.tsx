80583cb571619d108bc7256ffdbacee054e62a68
Krishnachandran U <kriss52450@gmail.com>
2024-05-31 00:27:52 +0530

    make the popup open and close

--- src/components/atoms/contactPopup.tsx
+++ src/components/atoms/contactPopup.tsx
@@ -1,6 +1,19 @@
-const ContactPopup = () => {
+import { IoIosClose } from "react-icons/io";
+
+const ContactPopup = (
+    {closePopup}:
+    {closePopup: () => void}
+) => {
     return (
-        <div className = "fixed z-[100] lg:w-[300px] h-[300px] top-0 left-0 -translate-x-[150px] -translate-y-[150px] bg-white shadow-lg">
+        <div className = "fixed w-screen h-screen flex justify-center items-center top-0 left-0">
+            <div className="w-[300px] h-[300px] bg-white shadow-2xl z-[100] transition-all duration-300 rounded-[32px] flex flex-col justify-between items-center p-[16px] gap-[8px]">
+                <button 
+                    onClick = {closePopup}
+                    className = "flex flex-row justify-end items-center w-full"
+                >
+                    <IoIosClose className = "text-[32px] text-[#202124] hover:text-black transition-all duration-300 cursor-pointer" />
+                </button>
+            </div>
         </div>
     );
 }
--- src/components/atoms/fab.tsx
+++ src/components/atoms/fab.tsx
@@ -1,8 +1,23 @@
+"use client"
+
+import { useState } from "react";
 import { Urbanist } from "next/font/google";
 
 const font = Urbanist({ subsets: ["latin"] });
 
+import ContactPopup from "./contactPopup";
+
 const Fab = () => {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const openPopup = () => {
+        setIsPopupOpen(true);
+    }
+
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    }
+
     return (
         <div className = {`p-[16px] ${font.className}`}>
             <div className = "rounded-[16px] border-[1px] shadow-lg transition-all hover:shadow-2xl duration-300 bg-white">
@@ -10,11 +25,17 @@ const Fab = () => {
                     <div>
                         Got an idea brewing in your mind? 
                     </div>
-                    <button className = "bg-[#202124] md:border-[4px] border-[2px] border-[#202124] hover:text-[#202124] hover:bg-white transition-all duration-300 hover:-translate-y-1 text-white py-[10px] px-[20px] rounded-[12px] font-extrabold whitespace-nowrap">
+                    <button 
+                        onClick = {openPopup}
+                        className = "bg-[#202124] md:border-[4px] border-[2px] border-[#202124] hover:text-[#202124] hover:bg-white transition-all duration-300 hover:-translate-y-1 text-white py-[10px] px-[20px] rounded-[12px] font-extrabold whitespace-nowrap"
+                    >
                         Get in touch!
                     </button>
                 </div>
             </div>
+            {isPopupOpen && (
+                <ContactPopup closePopup={closePopup}/>
+            )}
         </div>
     );
 }
