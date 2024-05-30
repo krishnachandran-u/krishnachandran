1d93235a1a1f1678dc1e293a74908caa4d728e91
Krishnachandran U <kriss52450@gmail.com>
2024-05-30 12:32:17 +0530

    increase font size of featured product description

--- src/components/atoms/worksSlide.tsx
+++ src/components/atoms/worksSlide.tsx
@@ -5,7 +5,7 @@ const WorksSlide = () => {
     return (
         <div className = "size-full flex flex-col gap-[24px] text-white group hover:cursor-pointer">
             <div className = "text-5xl font-semibold">Placeholder</div>
-            <div className = "text-[18px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</div>
+            <div className = "lg:text-[24px] sm:text-[20px] text-[18px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</div>
             <div className = "flex justify-center items-center sm:max-w-[300px] sm:max-h-[300px] max-w-[250px] max-h-[250px] size-full overflow-hidden">
                 <Image src = {sampleImg} alt = "Sample Image" className="group-hover:scale-110 transition-all duration-300 -translate-x-[2px]" />
             </div>
